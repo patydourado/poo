195b2deaee1d437d2f96d2ba717d66f3cb653585
Patrícia Dourado <patricia.almeida@pro.ucsal.br>
2026-04-14 16:36:48 -0300

    Create Circulo.java
    Versão aplicando o conceito de encapsulamento e uso de propriedades (métodos gets e sets quando considerar necessário)

---
+++ circulo02/Circulo.java
@@ -0,0 +1,24 @@
+package poo.ucsal.br;
+
+public class Circulo {
+   private int centroX, centroY;
+   private int raio;
+
+   public Circulo(int centroX, int centroY, int raio) {
+      this.centroX = centroX;
+      this.centroY = centroY;
+      this.raio = raio;
+   }
+
+   public double area() {
+      return Math.PI * raio * raio;
+   }
+
+  public int getRaio() {
+		return raio;
+	}
+
+	public void setRaio(int raio) {
+		this.raio = raio;
+	}
+}
