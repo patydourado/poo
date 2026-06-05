38974d3b1074ea25bf33975c3da645ce5d21cff6
Patrícia Dourado <patricia.almeida@pro.ucsal.br>
2026-06-05 12:32:00 -0300

    Create Pix.java

---
+++ polimorfismo/projeto-sem-polimorfismo/Pix.java
@@ -0,0 +1,7 @@
+class Pix {
+    public void processarPix(double valor) {
+        System.out.println("[PIX] Processando pagamento de R$ " + valor);
+        System.out.println("[PIX] QR Code gerado. Chave Pix validada. Pago instantaneamente!");
+    }
+}
+
