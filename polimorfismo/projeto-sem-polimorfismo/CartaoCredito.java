4431aeceb2acf3c7c8bb7eb490740685f9794107
Patrícia Dourado <patricia.almeida@pro.ucsal.br>
2026-06-05 12:34:00 -0300

    Create CartaoCredito.java

---
+++ polimorfismo/projeto-sem-polimorfismo/CartaoCredito.java
@@ -0,0 +1,6 @@
+class CartaoCredito {
+    public void processarCartao(double valor) {
+        System.out.println("[CARTÃO] Processando pagamento de R$ " + valor);
+        System.out.println("[CARTÃO] Conectando com a operadora... Transação autorizada!");
+    }
+}
